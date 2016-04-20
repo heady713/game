62d6c41731138ce7dc62f771584c82dbcc929ada
Nemo@MacOX <heady713@gmail.com>
2016-04-21 00:54:34 +0800

    精灵代码重构

--- test/js/sprite.js
+++ test/js/sprite.js
@@ -1,96 +1,192 @@
-var CANVAS = CANVAS || {};
+var GAME = GAME || {};
 // ===================================================
-// =====================::POINT::=====================
+// =====================::GLOBAL::====================
 // ===================================================
-CANVAS.Point = function(x, y) {
-    this.x = isNaN(x) ? 0 : x;
-    this.y = isNaN(y) ? 0 : y;
-}
-CANVAS.Point.prototype.clone = function() {
-    return new CANVAS.Point(this.x, this.y);
+GAME.canvas = document.createElement('canvas');
+GAME.canvas.width = 320;
+GAME.canvas.height = 640;
+GAME.context = GAME.canvas.getContext('2d');
+
+//保存所有Sprite
+GAME.children = {};
+GAME.childCount = 0;
+
+GAME.updateChildren = function() {
+    if (GAME.childCount > 0) {
+        GAME.context.clearRect(0, 0, GAME.canvas.width, GAME.canvas.height);
+        for (var k in GAME.children) {
+            var child = GAME.children[k];
+            GAME.context.drawImage(child.image, child.pos.x, child.pos.y, child.width, child.height);
+        };
+    }
 };
-CANVAS.Point.prototype.setPosition = function(x, y) {
-    this.x = isNaN(x) ? this.x : x;
-    this.y = isNaN(y) ? this.y : y;
+
+GAME.getDistance = function(pointA, pointB) {
+    return Math.sqrt(Math.pow(pointA.x - pointB.x, 2), Math.pow(pointA.y - pointB.y, 2))
 };
-CANVAS.Point.prototype.equals = function(point) {
-    return this.x == point.x && this.y == point.y;
-};
-CANVAS.Point.prototype.toString = function() {
-    return "{x:" + this.x + " , y:" + this.y + "}";
-};
+
 // ===================================================
 // =====================::Sprite::====================
 // ===================================================
-CANVAS.Sprite = function(src, width, height) {
-    this.canvas = document.createElement('canvas');
-    this.canvas.x = this.x = 0;
-    this.canvas.y = this.y = 0;
-    this.context = this.canvas.getContext('2d');
-    this.canvas.widht = this.width = width;
-    this.canvas.height = this.height = height;
-    this.center = {
-        x: this.x + 0.5 * this.width,
-        y: this.y + 0.5 * this.height
+GAME.Sprite = function(tag, src, width, height) {
+    this.tag = tag;
+    this.src = src;
+    this.width = width;
+    this.height = height;
+    this.pos = {
+        x: 0,
+        y: 0
     };
-    this.parentCanvas = null;
+    this.center = {
+        x: this.pos.x + 0.5 * this.width,
+        y: this.pos.y + 0.5 * this.height
+    };
+    this.scale = {
+        x: 1,
+        y: 1
+    };
+
     this.image = new Image();
-    this.image.src = src;
+    this.image.src = this.src;
     var self = this;
     this.image.onload = function() {
-        self.context.drawImage(self.image, 0, 0);
-        self.refresh();
+        GAME.children[tag] = self;
+        GAME.childCount++;
+        GAME.updateChildren();
     };
-};
-CANVAS.Sprite.prototype.addTo = function(canvas) {
-    this.parentCanvas = canvas;
-};
-CANVAS.Sprite.prototype.refresh = function() {
-    if (this.parentCanvas) {
-        var context = this.parentCanvas.getContext('2d');
-        context.globalCompositeOperation = "source-over";
-        context.drawImage(this.canvas, this.x, this.y);
+}
+
+GAME.Sprite.prototype.removeFromGlobal = function() {
+    if (GAME.children[this.tag] == undefined) {
+        console.log("tag:", this.tag, "isnot exists");
+        return;
     }
+    delete GAME.children[this.tag];
+    GAME.childCount--;
+    // GAME.updateChildren();
 };
-CANVAS.Sprite.prototype.setPosition = function(point) {
-    this.canvas.x = this.x = point.x;
-    this.canvas.y = this.y = point.y;
-    this.center.x = this.x + 0.5 * this.width;
-    this.center.y = this.y + 0.5 * this.height;
+
+GAME.Sprite.prototype.setPosition = function(x, y) {
+    this.pos.x = x;
+    this.pos.y = y;
+    this.center.x = this.pos.x + 0.5 * this.width;
+    this.center.y = this.pos.y + 0.5 * this.height;
 };
-CANVAS.Sprite.prototype.setCenterPosition = function(point) {
-    this.center.x = point.x;
-    this.center.y = point.y;
-    this.canvas.x = this.x = point.x - 0.5 * this.width;
-    this.canvas.y = this.y = point.y - 0.5 * this.height;
+
+GAME.Sprite.prototype.setCenterPosition = function(x, y) {
+    this.center.x = x;
+    this.center.y = y;
+    this.pos.x = x - 0.5 * this.width;
+    this.pos.y = y - 0.5 * this.height;
 };
-CANVAS.Sprite.prototype.setScale = function(scaleX, scaleY) {
-    this.context = this.canvas.getContext('2d');
-    this.context.clearRect(0, 0, this.canvas.widht, this.canvas.height);
-    this.context.scale(scaleX, scaleY);
-    this.context.drawImage(this.image, 0, 0);
-    var newWidth = this.width * scaleX;
-    var newHeight = this.height * scaleX;
-    var offsetX = (this.width - newWidth) / 4;
-    var offsetY = (this.height - newHeight) / 4;
-    this.width = newWidth;
-    this.height = newHeight;
-    this.setCenterPosition(new CANVAS.Point(this.center.x, this.center.y));
+
+GAME.Sprite.prototype.setScale = function(scaleX, scaleY) {
+    this.scale.x *= scaleX;
+    this.scale.y *= scaleY
+    this.width *= scaleX;
+    this.height *= scaleY;
 };
+
+
+// ===================================================
+// =====================::Demo::======================
+// ===================================================
+refreshDelay = 1000;
+// 主函数
+window.requestAnimationFrame = window.__requestAnimationFrame ||
+    //
+    window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
+    //
+    window.mozRequestAnimationFrame || window.oRequestAnimationFrame ||
+    //
+    window.msRequestAnimationFrame || (function() {
+        return function(callback) {
+            window.setTimeout(callback, refreshDelay);
+        };
+    })();
+
 var canvasContainer = document.getElementById('game');
-var g_canvas = document.createElement('canvas');
-g_canvas.width = 300;
-g_canvas.height = 800;
-g_canvas.style.position = "absolute";
-context = g_canvas.getContext("2d");
-canvasContainer.appendChild(g_canvas);
-var player = new CANVAS.Sprite('img/Soccer.png', 50, 50);
-player.addTo(g_canvas);
-player.setPosition(new CANVAS.Point(30, 80));
+canvasContainer.appendChild(GAME.canvas);
+
+var bg = new GAME.Sprite('bg','img/bg.jpg', 320, 640);
+
+
+var Player = function(tag, src, width, height) {
+   GAME.Sprite.apply(this, arguments);
+};
+var F = new Function();
+F.prototype = GAME.Sprite.prototype;
+Player.prototype = new F();
+Player.prototype.getX = function() {
+    return {x:this.pos.x, y:this.pos.y, cx:this.center.x, cy:this.center.y};
+};
+
+var player = new Player('player', 'img/jiaose_s.png', 56, 122);
+player.setCenterPosition(160, 320);
+
+
+var xl = 292,
+    yl = 308;
+var xd1 = 128,
+    xd2 = 592;
+var k = Math.abs((xl-xd1)/(yl-1280));
+
+var soccer1 = new GAME.Sprite('soccer1', 'img/Soccer.png', 40, 40);
+soccer1.setCenterPosition(160, 640);
+
+var soccer2 = new GAME.Sprite('soccer2', 'img/Soccer.png', 40, 40);
+soccer2.setCenterPosition(xd1*320/720, 640);
+
+var soccer3 = new GAME.Sprite('soccer3', 'img/Soccer.png', 40, 40);
+soccer3.setCenterPosition(xd2*320/720, 640);
+
+var step = 4;
+var minHeight = 180;
+
+var loop = function() {
+    if (soccer1) {
+        soccer1.setCenterPosition(soccer1.center.x, soccer1.center.y - step);
+        if (soccer1.center.y < minHeight) {
+            soccer1.removeFromGlobal();
+            soccer1 = null;
+        }
+    }
+    if (soccer2) {
+        soccer2.setCenterPosition(soccer2.center.x + step * k, soccer2.center.y - step);
+        if (soccer2.center.y < minHeight) {
+            soccer2.removeFromGlobal();
+            soccer2 = null;
+        }
+    }
+
+    if (soccer3) {
+        soccer3.setCenterPosition(soccer3.center.x - step * k, soccer3.center.y - step);
+        if (soccer3.center.y < minHeight) {
+            soccer3.removeFromGlobal();
+            soccer3 = null;
+        }
+    }
+    GAME.updateChildren();
+    requestAnimationFrame(loop);
+}
+
 var btn = document.getElementById('btn');
 btn.onclick = function(e) {
-    context.clearRect(0, 0, g_canvas.width, g_canvas.height);
-    player.setScale(0.9, 0.9);
-    player.setPosition(new CANVAS.Point(Math.random() * 100, Math.random() * 80));
-    player.refresh();
+    requestAnimationFrame(loop);
 };
+
+var Player = function(tag, src, width, height) {
+   GAME.Sprite.apply(this, arguments);
+};
+var F = new Function();
+F.prototype = GAME.Sprite.prototype;
+Player.prototype = new F();
+Player.prototype.getX = function() {
+    return {x:this.pos.x, y:this.pos.y, cx:this.center.x, cy:this.center.y};
+};
+
+var player = new Player('player', 'img/jiaose_s.png', 56, 122);
+player.setCenterPosition(160, 320);
+console.log(player.getX());
+
+//TODO  增加zorder 改变元素绘制的先后顺序 影响其层级

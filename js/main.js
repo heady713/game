1a9be4df70432f2176ce340cf74d03662bb5055b
Nemo@MacOX <heady713@gmail.com>
2016-04-23 11:22:13 +0800

    fix jump

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var player, shadow, monsters = [],
     asideMiles = [];
 var winWidth, winHeight, isGuide = false;
 var startTouchPoint;
-var touchCacheX = 0.25,
+var touchCacheX = 0.15,
     touchCacheY = 0.2;
 var startTime,
     refreshDelay = 24,
@@ -75,14 +75,14 @@ var initStage = function() {
     var len = winHeight > winWidth ? winWidth : winHeight;
     myTouch.on('swipe', function(e) {
         if (e.moveX >= len * touchCacheX) { //right
-            if (!player.moving) {
+            if (!player.moving && !player.jumping) {
                 player.moving = true;
                 player.moveDirect = 1;
                 shadow.moving = true;
                 shadow.moveDirect = 1;
             }
         } else if (e.moveX <= -len * touchCacheX) { //left
-            if (!player.moving) {
+            if (!player.moving && !player.jumping) {
                 player.moving = true;
                 player.moveDirect = -1;
                 shadow.moving = true;
@@ -117,12 +117,12 @@ var resizeHandler = function() {
 var resetStage = function() {
     winWidth = $(canvasContainer).width();
     winHeight = $(canvasContainer).height();
-    DF.M.maxPath = getScaleY(HEIGHT - yl);
-    DF.M.maxPathMile = getScaleY(HEIGHT - yl);
+    DF.M.maxPath = getScaleY(HEIGHT - yl - 25);
+    DF.M.maxPathMile = getScaleY(HEIGHT - yl - 25);
     DF.M.moveSpeed = winHeight * 0.009;
     DF.P.moveSpeed = winHeight * 0.006;
     var k = Math.abs((getScaleX(xl) - getScaleX(xd1)) / (winHeight - getScaleY(yl)));
-    DF.P.pathWidth = DF.M.maxPath / 10 * 7 * k;
+    DF.P.pathWidth = DF.M.maxPath / 10 * 6 * k;
     if (GAME.canvas) {
         canvasContainer.removeChild(GAME.canvas);
     }

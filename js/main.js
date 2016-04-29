fa545f95613a76d06b4ba5df914ce4e95b09dd00
shyeun <evenshry@163.com>
2016-04-29 16:37:48 +0800

    test

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ var winWidth, winHeight, guideStatus = 0,
 var startTouchPoint, touchCacheX = 0.15,
     touchCacheY = 0.2;
 var startTime, pauseTime, gmfCounts = 0,
-    stepLength = 2000;
+    stepLength = 500;
 // 初始化页面
 $(function() {
     loadPlayerCnt();
